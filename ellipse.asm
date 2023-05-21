dcd486e3e7b2eac9fd8c4c8d1d44ac7dfb1529fe
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2023-05-21 17:48:20 +0200

    Command line arguments

--- ellipse.asm
+++ ellipse.asm
@@ -1,7 +1,10 @@
 OPTION SCOPED
 
 data1 segment
-    vga   dw 0a000h
+    vga          dw 0a000h
+    argsOffset   dw 80h
+
+    errorMessage db "Blad danych wejsciowych$"
 data1 ends
 
 code1 segment
@@ -9,21 +12,42 @@ code1 segment
 main proc
 
     setupStack:  
-                 mov  ax, seg stack1                 ; setup stack1 as stack segment
+                 mov  ax, seg stack1                  ; setup stack1 as stack segment
                  mov  ss, ax
-                 mov  sp, offset s1top               ; setup stack pointer (top of the stack)
+                 mov  sp, offset s1top                ; setup stack pointer (top of the stack)
 
     setupData:   
-                 mov  ax, seg data1                  ; setup data1 as data segment
+                 mov  ax, seg data1                   ; setup data1 as data segment
                  mov  ds, ax
+
+    setupPSP:    
+                 mov  si, word ptr ds:[argsOffset]
+                 inc  si
+
+                 call skipSpaces
+                 call parseNumber
+                 call ceilDiv
+                 mov  word ptr cs:[ellipseX], ax
+
+                 call skipSpaces
+                 call parseNumber
+                 call ceilDiv
+                 mov  word ptr cs:[ellipseY], ax
+                
+                 call skipSpaces
+                
+                 cmp  al, 0
+                 je   setupVGA
+
+                 cmp  al, 13
+                 je   setupVGA
+
+                 jmp  handleError
 
     setupVGA:    
                  mov  ah, 0h
-                 mov  al, 13h                        ; 320x200, 256 colors
+                 mov  al, 13h                         ; 320x200, 256 colors
                  int  10h
-
-                 mov  word ptr cs:[ellipseX], 60
-                 mov  word ptr cs:[ellipseY], 40
 
     redraw:      
                  call nowDrawing
@@ -59,23 +83,23 @@ main proc
 
     upArrow:     
                  cmp  word ptr cs:[ellipseY], 100
-                 je   mainLoop
+                 jae  mainLoop
                  call nowErasing
                  call drawEllipse
                  add  word ptr cs:[ellipseY], 1
                  jmp  redraw
 
     downArrow:   
-                 cmp  word ptr cs:[ellipseY], 2
-                 je   mainLoop
+                 cmp  word ptr cs:[ellipseY], 1
+                 jle  mainLoop
                  call nowErasing
                  call drawEllipse
                  sub  word ptr cs:[ellipseY], 1
                  jmp  redraw
 
     leftArrow:   
-                 cmp  word ptr cs:[ellipseX], 2
-                 je   mainLoop
+                 cmp  word ptr cs:[ellipseX], 1
+                 jle  mainLoop
                  call nowErasing
                  call drawEllipse
                  sub  word ptr cs:[ellipseX], 1
@@ -83,7 +107,7 @@ main proc
 
     rightArrow:  
                  cmp  word ptr cs:[ellipseX], 160
-                 je   mainLoop
+                 jae  mainLoop
                  call nowErasing
                  call drawEllipse
                  add  word ptr cs:[ellipseX], 1
@@ -131,22 +155,22 @@ sine proc
 
     calculate:   
                  mov  bx, 360
-                 sub  bx, ax                         ; bx = 360 - alpha
-                 mul  bx                             ; dx:ax = alpha(360 - alpha)
+                 sub  bx, ax                          ; bx = 360 - alpha
+                 mul  bx                              ; dx:ax = alpha(360 - alpha)
                  push dx
                  push ax
 
                  mov  cx, 4
-                 div  cx                             ; dx:ax = alpha(360 - alpha)/4
+                 div  cx                              ; dx:ax = alpha(360 - alpha)/4
                  mov  cx, 40500
-                 sub  cx, ax                         ; cx = 40500 - alpha(360 - alpha)/4
+                 sub  cx, ax                          ; cx = 40500 - alpha(360 - alpha)/4
                 
                  pop  ax
-                 pop  dx                             ; dx:ax = alpha(360 - alpha)
+                 pop  dx                              ; dx:ax = alpha(360 - alpha)
 
                  mov  bx, 1000
-                 mul  bx                             ; dx:ax = 1000*alpha(360 - alpha)
-                 div  cx                             ; dx:ax = dx:ax/cx
+                 mul  bx                              ; dx:ax = 1000*alpha(360 - alpha)
+                 div  cx                              ; dx:ax = dx:ax/cx
 
     exit:        
                  pop  cx
@@ -236,6 +260,90 @@ nowErasing proc
     erasingColor db   0
 
 nowErasing endp
+
+handleError proc
+
+                 mov  dx, offset errorMessage
+                 mov  ah, 09h
+                 int  21h
+
+                 mov  ax, 4c01h
+                 int  21h
+
+handleError endp
+
+skipSpaces proc
+                
+    loop1:       
+                 mov  al, byte ptr es:[si]
+                 inc  si
+                
+                 cmp  al, ' '
+                 jne  exit
+
+                 jmp  loop1
+
+    exit:        
+                 dec  si
+                 ret
+
+skipSpaces endp
+
+parseNumber proc
+
+    init:        
+                 mov  ax, 0
+                 mov  dx, 0
+                 mov  cx, 0
+
+    loop1:       
+                 mov  bl, byte ptr es:[si]
+                 inc  si
+
+                 cmp  bl, ' '
+                 je   exit
+
+                 cmp  bl, 0
+                 je   exit
+
+                 cmp  bl, 13
+                 je   exit
+
+                 cmp  bl, '9'
+                 ja   handleError
+
+                 cmp  bl, '0'
+                 jb   handleError
+
+                 mov  cx, 10
+                 mul  cx
+
+                 mov  bh, 0
+                 sub  bx, '0'
+                 add  ax, bx
+
+                 cmp  ax, 200
+                 ja   handleError
+
+                 jmp  loop1
+
+    exit:        
+                 cmp  cx, 0
+                 je   handleError
+
+                 ret
+
+parseNumber endp
+
+ceilDiv proc
+
+                 mov  dx, 0
+                 mov  cx, 2
+                 div  cx
+                 add  ax, dx
+                 ret
+
+ceilDiv endp
 
 code1 ends
 
